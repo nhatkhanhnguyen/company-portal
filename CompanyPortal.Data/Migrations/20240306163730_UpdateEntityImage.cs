c337459f775c70ec460dfb191605c1dcef13be38
Galvin Nguyen <khanh.nguyennhat93@gmail.com>
2024-03-06 23:38:31 +0700

    Add migraiton

---
+++ CompanyPortal.Data/Migrations/20240306163730_UpdateEntityImage.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CompanyPortal.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEntityImage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Articles_ArticleId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_ArticleId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ArticleId",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ExternalId",
+                table: "Products",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ExternalId",
+                table: "Categories",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(5)",
+                oldMaxLength: 5);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Articles",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Articles",
+                type: "nvarchar(2000)",
+                maxLength: 2000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Articles",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Articles");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ExternalId",
+                table: "Products",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ArticleId",
+                table: "Products",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ExternalId",
+                table: "Categories",
+                type: "nvarchar(5)",
+                maxLength: 5,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Articles",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Articles",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2000)",
+                oldMaxLength: 2000);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ArticleId",
+                table: "Products",
+                column: "ArticleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Articles_ArticleId",
+                table: "Products",
+                column: "ArticleId",
+                principalTable: "Articles",
+                principalColumn: "Id");
+        }
+    }
+}
